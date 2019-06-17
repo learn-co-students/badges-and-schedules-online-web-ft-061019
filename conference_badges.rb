eb5a0ee816c29f55191c9080997c8ef2fc75816b
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-06-17 22:06:42 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def batch_badge_creator(badges)
+  badges = []
+  badges.each { |name| badges << name} 
+  name = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', and 'Matz']
+  puts "Hello, #{badges}! You'll be assigned to room" 
+  end 
+  badges
+end 
